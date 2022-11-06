b62e8d14de455e12e6ae99942dd8be8778bf0186
kuldeep.singh <kuldeepnaruka934@gmail.com>
2022-11-06 16:08:05 +0530

    I solve the question with full explaination

--- question_5.py
+++ question_5.py
@@ -1,6 +1,24 @@
-#
-def total_students_length(students):
-    print(" input is this ", students)
-    total_length = len(students)
+"""question - How many students are in the "students" dict? Use the appropriate function."""
 
+"""here I am creating a new function and its name is students_length and in function  i passed a dictionary named students
+as parameter """
+def students_length(students):
+    """ Here I print the input dictionary because when it prints I see the dictionary, so It's easy to calculate the
+        length"""
+    print(" input is this ", students)
+    """Here I am using len() function to determine the length of dictionary and I assign the value of len() 
+         function in a variable name total_length"""
+    total_length = len(students)
+    """Here I return the length to get output of the above function"""
     return total_length
+
+
+students = {
+    "Peter": {"age": 10, "address": "Lisbon"},
+    "Isabel": {"age": 11, "address": "Sesimbra"},
+    "Anna": {"age": 9, "address": "Lisbon"},
+}
+"""Here I called function and assign the value of function in a variable"""
+length_of_dictionary_students= students_length(students)
+"""here I am calling the variable in which function was assigned"""
+print(length_of_dictionary_students)
