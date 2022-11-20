083a65100bc9ef71e5d27271a2f38eabe59213c2
kuldeep.singh <kuldeepnaruka934@gmail.com>
2022-11-20 20:47:29 +0530

    here i solve question -6 in which i find average of dictionary e Please enter the commit message for your changes. Lines starting

---
+++ question_6.py
@@ -0,0 +1,44 @@
+# def function_2(students, x):
+#     new_dict = {}
+#     for p, q in students.items():
+#         # print(p, q)
+#         if x == q.get('address'):
+#             new_dict[p]=x
+#     return new_dict
+#
+
+
+#
+# print(function_2(students, x= 'Lisbon'))
+
+
+#
+# def find_length(student):
+#     for key, value in student.items():
+#         len()
+#         print(key, value)
+#
+#
+# student = {
+#     "Peter": {"age": 10, "address": "Lisbon"},
+#     "Isabel": {"age": 11, "address": "Sesimbra"},
+#     "Anna": {"age": 9, "address": "Lisbon"},
+# }
+#
+# print(find_length(student))
+
+def average_students(students):
+    total = 0
+    for i in students.keys():
+        total += students[i]['age']
+
+    average = total / len(students.keys())
+    return average
+
+
+students = {
+    "Peter": {"age": 10, "address": "Lisbon"},
+    "Isabel": {"age": 11, "address": "Sesimbra"},
+    "Anna": {"age": 9, "address": "Lisbon"},
+}
+print(average_students(students))
